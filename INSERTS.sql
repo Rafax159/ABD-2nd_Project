bc42eab4bd4abd62a6778ca3c15452b9030e6ded
Revan2421 <luis.guti2401@gmail.com>
2025-03-03 16:52:47 -0400

    Inserciones

---
+++ INSERTS.sql
@@ -0,0 +1,30 @@
+INSERT INTO Pais VALUES 
+('Venezuela', 'Venezuela', '+58'), 
+('Colombia', 'Colombia', '+57'), 
+('Argentina', 'Argentina', '+54'),
+('Italia', 'Italy', '+39'),
+('Alemania', 'Germany', '+49');
+
+INSERT INTO Ciudad VALUES
+('Caracas', 'Caracas', '1060', 'Venezuela'),
+('Coro', 'Coro', '4101', 'Venezuela'),
+('Medellin', 'Medellin', '050005', 'Colombia'),
+('Cartagena', 'Cartagena', '13005', 'Colombia'),
+('Buenos Aires', 'Buenos Aires', 'B1043', 'Argentina'),
+('Mar de Plata', 'Mar de Plata', 'B1414', 'Argentina'),
+('Roma', 'Rome', '00127', 'Italia'),
+('Venecia', 'Venice', '30124', 'Italia'),
+('Berlin', 'Berlin', '10178', 'Alemania'),
+('Munich', 'Munich', '80336', 'Alemania');
+
+INSERT INTO Entranador VALUES
+(001, 'Luis', 'Garcia', 4129916677, 'luis@ejemplo.com', 'M', '24/01/00', 'Coro'),
+(002, 'Rafael', 'Contreras', 4242550835, 'rafael@ejemplo.com', 'M', '01/08/02', 'Caracas'),
+(003, 'Marcel', 'Mendoza', 3001234567, 'marcel@ejemplo.com', 'M', '26/12/00', 'Medellin'),
+(004, 'Julia', 'Gomez', 6012345678, 'julia@ejemplo.com', 'F', '12/02/05', 'Cartagena'),
+(006, 'Facundo', 'Gimenez', 1112345678, 'facundo@ejemplo.com', 'M', '10/03/06', 'Buenos Aires'),
+(007, 'Carmen', 'Ramos', 3511234567, 'carmen@ejemplo.com', 'F', '15/08/02', 'Mar de Plata'),
+(008, 'Luca', 'Rossi', 3331234567, 'luca@ejemplo.com', 'M', '08/01/03', 'Roma'),
+(009, 'Sofia', 'Bianchi', 0212345678, 'sofia@ejemplo.com', 'F', '10/01/00', 'Venecia'),
+(010, 'Hans', 'Müller', 1711234567, 'hans@ejemplo.com', 'M', '24/01/00', 'Berlin'),
+(011, 'Emma', 'Schmidt', 3012345678, 'emma@ejemplo.com', 'M', '24/01/00', 'Munich');
