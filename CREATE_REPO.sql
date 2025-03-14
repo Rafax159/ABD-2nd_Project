a4c5bfde3c4f01b74f6235db777d5b78e37a87a6
Rafael <rafaelcontrerasagudelo@gmail.com>
2025-03-14 16:59:06 -0400

    Repo, usuario, roles, tablas y triggers en un mismo sql (create_tables)

--- CREATE_REPO.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLESPACE repo_tablas DATAFILE 'df_tablas_digi.DFB' SIZE 500M;
-CREATE TABLESPACE repo_indices DATAFILE 'df_indices_digi.DFB' SIZE 500M;
--- CREATE_TABLES.sql
+++ CREATE_TABLES.sql
@@ -1,3 +1,56 @@
+/**************** CREACION DEL REPOSITORIO ***************/
+
+CREATE TABLESPACE repo_tablas DATAFILE 'df_tablas_digi.DFB' SIZE 500M;
+CREATE TABLESPACE repo_indices DATAFILE 'df_indices_digi.DFB' SIZE 500M;
+CREATE TABLESPACE usuarios_esquema DATAFILE 'df_usuarios_esq.DFB' SIZE 500M;
+
+--DROP TABLESPACE repo_tablas INCLUDING CONTENTS AND DATAFILES;
+--DROP TABLESPACE repo_indices INCLUDING CONTENTS AND DATAFILES;
+--DROP TABLESPACE usuarios_esquema INCLUDING CONTENTS AND DATAFILES;
+
+/*********************** USUARIOS Y ROLES ******************************/
+
+ALTER SESSION SET "_ORACLE_SCRIPT"=TRUE;
+
+CREATE USER Revan IDENTIFIED BY 12345 DEFAULT TABLESPACE usuarios_esquema;
+CREATE USER Nihilus IDENTIFIED BY 12345 DEFAULT TABLESPACE usuarios_esquema;
+CREATE USER Vader IDENTIFIED BY 12345 DEFAULT TABLESPACE usuarios_esquema;
+
+CREATE ROLE Registrador;
+CREATE ROLE Moderador;
+CREATE ROLE Administrador;
+
+GRANT INSERT, UPDATE ON Entrenador TO Registrador;
+GRANT INSERT, UPDATE ON Digimon TO Registrador;
+
+GRANT SELECT ON Pais TO Moderador;
+GRANT SELECT ON Ciudad TO Moderador;
+GRANT SELECT ON Entranador TO Moderador;
+GRANT SELECT ON Digimon TO Moderador;
+GRANT SELECT ON Naturaleza TO Moderador;
+GRANT SELECT ON Habilidad_Esp TO Moderador;
+GRANT SELECT ON Tipo_Digimon TO Moderador;
+GRANT SELECT ON Digievoluciona TO Moderador;
+GRANT SELECT ON Entrena TO Moderador;
+
+GRANT INSERT, UPDATE, DELETE, SELECT ON Pais TO Administrador;
+GRANT INSERT, UPDATE, DELETE, SELECT ON Ciudad TO Administrador;
+GRANT INSERT, UPDATE, DELETE, SELECT ON Entrenador TO Administrador;
+GRANT INSERT, UPDATE, DELETE, SELECT ON Digimon TO Administrador;
+GRANT INSERT, UPDATE, DELETE, SELECT ON Naturaleza TO Administrador;
+GRANT INSERT, UPDATE, DELETE, SELECT ON Habilidad_Esp TO Administrador;
+GRANT INSERT, UPDATE, DELETE, SELECT ON Tipo_Digimon TO Administrador;
+GRANT INSERT, UPDATE, DELETE, SELECT ON Digievoluciona TO Administrador;
+GRANT INSERT, UPDATE, DELETE, SELECT ON Entrena TO Administrador;
+
+GRANT CREATE SESSION, Registrador TO Revan;
+GRANT CREATE SESSION, Moderador TO Nihilus;
+GRANT CREATE SESSION, Administrador TO Vader;
+
+--CONNECT SUPER_USUARIO/12345;
+
+/**************** CREACION DEL ESQUEMA ******************/
+
 CREATE TABLE Pais (
     nombre_ESP VARCHAR2(30) NOT NULL,
     nombre_EN VARCHAR2(30) NOT NULL,
@@ -8,7 +61,7 @@ CREATE UNIQUE INDEX ind_nombre_pais_indx ON Pais(nombre_ESP) TABLESPACE repo_ind
 ALTER TABLE Pais ADD CONSTRAINT nombre_pais_PK PRIMARY KEY (nombre_ESP);
 
 
-CREATE TABLE Ciudad(
+CREATE TABLE Ciudad (
     nombre_ESP VARCHAR2(30) NOT NULL,
     nombre_EN VARCHAR2(30) NOT NULL,
     codigo_postal VARCHAR2(10) NOT NULL,
@@ -16,7 +69,7 @@ CREATE TABLE Ciudad(
     CONSTRAINT pais_ciudad_FK FOREIGN KEY (pais) REFERENCES Pais(nombre_ESP)
 )TABLESPACE repo_tablas;
 
-CREATE UNIQUE INDEX ind_nombre_ciudad_PK ON Ciudad(nombre_ESP) TABLESPACE repo_indices;
+CREATE UNIQUE INDEX ind_nombre_ciudad_indx ON Ciudad(nombre_ESP) TABLESPACE repo_indices;
 ALTER TABLE Ciudad ADD CONSTRAINT nombre_ciudad_PK PRIMARY KEY (nombre_ESP);
 
 
@@ -32,7 +85,7 @@ CREATE TABLE Entrenador (
     CONSTRAINT ciudad_origen_FK FOREIGN KEY (ciudad_origen) REFERENCES Ciudad(nombre_ESP)
 )TABLESPACE repo_tablas;
 
-CREATE UNIQUE INDEX ind_entrenador_PK ON Entrenador(id_entrenador) TABLESPACE repo_indices;
+CREATE UNIQUE INDEX ind_entrenador_indx ON Entrenador(id_entrenador) TABLESPACE repo_indices;
 CREATE INDEX ind_nombre_entrenador ON Entrenador(nombre) TABLESPACE repo_indices;
 ALTER TABLE Entrenador ADD CONSTRAINT id_entrenador_PK PRIMARY KEY (id_entrenador);
 
@@ -50,10 +103,9 @@ CREATE TABLE Digimon(
     ataque NUMBER(4) NOT NULL CHECK (ataque>=0 AND ataque<=9999),
     ataque_especial NUMBER(4) NOT NULL CHECK (ataque_especial>=0 AND ataque_especial<=9999),
     defensa_especial NUMBER(4) NOT NULL CHECK (defensa_especial>=0 AND defensa_especial<=9999),
-
-    CONSTRAINT digimon_naturaleza_fk FOREIGN KEY (naturaleza) REFERENCES Naturaleza (nombre),
-    CONSTRAINT digimon_habilidadEsp_fk FOREIGN KEY (habilidad_especial) REFERENCES Habilidad_Esp (nombre),
-    CONSTRAINT digimon_tipo_fk FOREIGN KEY (tipo) REFERENCES Tipo_Digimon (nombre)
+    CONSTRAINT Digimon_Naturaleza_fk FOREIGN KEY (naturaleza) REFERENCES Naturaleza(nombre),
+    CONSTRAINT Digimon_tipo_fk FOREIGN KEY (tipo) REFERENCES Tipo_Digimon(nombre),
+    CONSTRAINT Digimon_Habilidad_fk FOREIGN KEY (habilidad_especial) REFERENCES Habilidad_Esp(nombre)
 )TABLESPACE repo_tablas;
 
 CREATE UNIQUE INDEX nombre_Digimon_indx ON Digimon (nombre) TABLESPACE repo_indices;
@@ -63,8 +115,8 @@ ALTER TABLE Digimon ADD CONSTRAINT nombre_digimon_pk PRIMARY KEY (nombre);
 CREATE TABLE Naturaleza(
     nombre VARCHAR(30),
     descripcion VARCHAR2(50),
-    beneficio VARCHAR2(200),
-    desventaja VARCHAR2(200)
+    beneficio VARCHAR2(20),
+    desventaja VARCHAR2(20)
 )TABLESPACE repo_tablas;
 
 CREATE UNIQUE INDEX nombre_Naturaleza_indx ON Naturaleza (nombre) TABLESPACE repo_indices;
@@ -92,13 +144,13 @@ ALTER TABLE Tipo_Digimon ADD CONSTRAINT nombre_tipoDigimon_pk PRIMARY KEY (nombr
 CREATE TABLE Digievoluciona(
     digimon_BASE VARCHAR2(30),
     digimon_EVO VARCHAR2(30),
-    tipo_Evo VARCHAR2(30),
+    tipo_Evo VARCHAR2(30) UNIQUE,
     CONSTRAINT digievoluciona_base_fk FOREIGN KEY (digimon_BASE) REFERENCES Digimon (nombre),
-    CONSTRAINT digievoluciona_evo_fk FOREIGN KEY (digimon_EVO) REFERENCES Digimon (nombre),
-    CONSTRAINT uni_tipoEvolucion UNIQUE (digimon_BASE,tipo_Evo)
+    CONSTRAINT digievoluciona_evo_fk FOREIGN KEY (digimon_EVO) REFERENCES Digimon (nombre)
 )TABLESPACE repo_tablas;
 
 ALTER TABLE Digievoluciona ADD CONSTRAINT digievoluciona_pk PRIMARY KEY (digimon_BASE,digimon_EVO);
+
 
 CREATE TABLE Entrena(
     id_entrenador NUMBER(10),
@@ -111,20 +163,8 @@ CREATE TABLE Entrena(
 
 ALTER TABLE Entrena ADD CONSTRAINT entrena_pk PRIMARY KEY (id_entrenador,nombre_digimon);
 
---DROP TRIGGER tr_max_digimon_entrenador;
 
-DROP TABLE Pais;
-DROP TABLE Ciudad;
-DROP TABLE Entrenador;
-DROP TABLE Naturaleza;
-DROP TABLE Digimon;
-DROP TABLE Habilidad_Esp;
-DROP TABLE Tipo_Digimon;
-DROP TABLE Digievoluciona;
-DROP TABLE Entrena;
-
---INSERT INTO ENTRENADOR VALUES (100, 'Luis', 'Garcia', 04129916677, 'luis@ejemplo.com', 'M', '24/01/15', 'Coro', '05/01/22'); -- Dara Error por la edad del entrenador
-
+--TRIGGER--
 CREATE OR REPLACE TRIGGER trg_max_digimones
 BEFORE INSERT OR UPDATE ON Entrena
 FOR EACH ROW
@@ -149,9 +189,21 @@ CREATE OR REPLACE TRIGGER tr_entrenador_edad
 BEFORE INSERT ON Entrenador
 FOR EACH ROW
 BEGIN
-    IF (EXTRACT(YEAR FROM (SYSDATE)) - EXTRACT(YEAR FROM (:NEW.fecha_nacimiento)) < 18) 
-    THEN
+    IF (EXTRACT(YEAR FROM (SYSDATE)) - EXTRACT(YEAR FROM (:NEW.fecha_nacimiento)) < 18) THEN
         RAISE_APPLICATION_ERROR(-20001,'Los Entrenadores deben ser mayor de edad...');
     END IF;
 END;
 /
+
+--DROP TRIGGER tr_entrenador_edad;
+--DROP TRIGGER tr_max_digimon_entrenador;
+
+/*DROP TABLE Pais;
+DROP TABLE Ciudad;
+DROP TABLE Entrenador;
+DROP TABLE Naturaleza;
+DROP TABLE Digimon;
+DROP TABLE Habilidad_Esp;
+DROP TABLE Tipo_Digimon;
+DROP TABLE Digievoluciona;
+DROP TABLE Entrena;*/
